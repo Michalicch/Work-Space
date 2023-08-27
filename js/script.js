40ba394d90b16137485baa7ca51adb49ba5d0dff
Michalicch <lorka.diskaveri@gmail.com>
2023-08-27 11:39:58 +0300

    Заполнение селекта с сервера

--- js/script.js
+++ js/script.js
@@ -1,5 +1,57 @@
-const citySelect = document.getElementById('city');
-const cityChoices = new Choices(citySelect, {
-	searchEnabled: false,
-	itemSelectText: '',
-})
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
+
+
+
+
+
+const getData = async (url, cbSuccess, cbError) => {
+	try {
+		const response = await fetch(url);
+		const data = await response.json();
+		cbSuccess(data);
+	} catch (error) {
+		cbError(error)
+	}
+}
+
+const init = () => {
+	const citySelect = document.getElementById('city');
+	const cityChoices = new Choices(citySelect, {
+		// searchEnabled: false,
+		itemSelectText: '',
+	})
+
+	getData(
+		`${API_URL}${LOCATION_URL}`,
+		(locationData) => {
+			const locations = locationData.map((location) =>({
+				value: location,
+			}));
+			cityChoices.setChoices(
+				locations,
+				"value",
+				"label",
+				false,
+			);
+
+			console.log();
+		}, (error) => {
+			console.log(error + 'Произошла ошибка!!!');
+		},
+	);
+}
+
+init();
+
+
+// fetch(API_URL + LOCATION_URL)
+// .then((response)=>{
+// 	return response.json();
+// })
+// .then((data)=>{
+// 	console.log(data);
+// })
+// .catch((error=>{
+// 	console.log(error + 'Произошла ошибка!!!');
+// }))
