4dec0aa10b705fd6c68b8424f82516d9d24ece5d
Michalicch <lorka.diskaveri@gmail.com>
2023-08-28 22:29:11 +0300

    Динамическая загрузка модального окна по нажатию на вакансию

--- js/modal.js
+++ js/modal.js
@@ -1,20 +1,20 @@
-const modal = () => {
-	const modal = document.querySelector('.modal')
-	const modalBtns = document.querySelectorAll('.cards__item')
+// const modal = () => {
+// 	const modal = document.querySelector('.modal')
+// 	const modalBtns = document.querySelectorAll('.cards__item')
 
 
-	modalBtns.forEach((item) => {
-		item.addEventListener('click', () => {
-			modal.classList.add('activ')  
-		})
-	})
-	modal.addEventListener('click', ()=>{
-		setTimeout(() =>{
-			modal.classList.remove('activ')
-		}, 1000)
+// 	modalBtns.forEach((item) => {
+// 		item.addEventListener('click', () => {
+// 			modal.classList.add('activ')  
+// 		})
+// 	})
+// 	modal.addEventListener('click', ()=>{
+// 		setTimeout(() =>{
+// 			modal.classList.remove('activ')
+// 		}, 1000)
 		
-	})
+// 	})
 
-}
+// }
 
-modal();
+// modal();
